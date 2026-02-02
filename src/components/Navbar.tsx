e8906c9e0dd8796fdd427250da9f16ac7e4eb574
Tulashi <tulashi58@gmail.com>
2026-02-01 21:39:55 -0500

    Skeleton creation

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+	return (
+		<nav>
+			<div>
+				<div>US EST</div>
+				<div>Left Menu</div>
+				<div>Home Logo</div>
+				<div>Right Menu</div>
+				<div>NST</div>
+			</div>
+		</nav>
+	)
+}
+
+export default Navbar
