5456c44e8e878121fd441d73007b3c1a01dffe00
roan1088 <rohan_188@yahoo.com>
2026-02-06 20:00:55 -0500

    organized files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter } from 'react-router-dom'
 import './App.css'
-import Footer from './components/Footer'
-import Home from './components/Home'
-import Navbar from './components/Navbar'
+import Footer from './components/layout/Footer'
+import Home from './pages/Home'
+import Navbar from './components/layout/Navbar'
 
 function App() {
 	return (
